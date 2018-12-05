55ba1dce9268f569c78ff37859ea0552a9c28537
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-04 21:50:54 -0500

    Create downloads.ps1

---
+++ poshcode/downloads.ps1
@@ -0,0 +1,65 @@
+$PageTitle   = ""
+$PageCaption = "Tools and Downloads"
+
+@"
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="$STTheme"/>
+</head>
+
+<body>
+
+<h1>$PageCaption</h1>
+
+<table id=table2>
+	<tr>
+		<td style="width:50%; vertical-align:top">
+			<h2>ConfigMgr Resources</h2>
+
+			<ul class="ulmenu">
+				<li class="limain"><a href="http://www.scconfigmgr.com/driver-automation-tool/" target="_new">Driver Automation Tool</a></li>
+				<li class="limain"><a href="http://www.scconfigmgr.com/category/console-extension/" target="_new">ConfigMgr Console Extension</a></li>
+				<li class="limain">Anders Rodland: Client Health Script</li>
+			</ul>
+
+		</td>
+		<td style="vertical-align:top">
+			<h2>Software Updates</h2>
+
+			<ul class="ulmenu">
+				<li class="limain">Dam Good Admin: WSUS Configuration Script</li>
+			</ul>
+		</td>
+	</tr>
+	<tr>
+		<td style="width:50%; vertical-align:top">
+			<h2>Windows Deployment</h2>
+
+			<ul class="ulmenu">
+				<li class="limain"><a href="https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install" target="_new">Windows ADK</a></li>
+				<li class="limain"><a href="https://docs.microsoft.com/en-us/sccm/mdt/" target="_new">Microsoft Deployment Toolkit (MDT)</a></li>
+				<li class="limain">David Segura: OSBuilder</li>
+				<li class="limain">Phil Pritchett: RunOSBuilder</li>
+			</ul>
+        </td>
+        <td style="vertical-align:top">
+			<h2>SQL Server</h2>
+
+			<ul class="ulmenu">
+				<li class="limain"><a href="https://ola.hallengren.com/" target="_blank">Ola Hallengren: DB Tools</a></li>
+				<li class="limain"><a href="https://docs.microsoft.com/en-us/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-2017" target="_new">SQL Server Management Studio</a></li>
+			</ul>
+
+			<h2>Other</h2>
+
+			<ul class="ulmenu">
+				<li class="limain"><a href="https://code.visualstudio.com/" target="_new" title="Visual Studio Code">Visual Studio Code</a></li>
+				<li class="limain"><a href="https://chocolatey.org" target="main" title="Chocolatey">Chocolatey</a></li>
+			</ul>        </td>
+    </tr>
+</table>
+
+</body>
+
+</html>
+"@
