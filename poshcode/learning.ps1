06c444551fda8bab818acd834891d59873dc5ad4
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-04 21:46:31 -0500

    Create learning.ps1

---
+++ poshcode/learning.ps1
@@ -0,0 +1,39 @@
+$PageTitle   = "Learning Resources"
+$PageCaption = "Learning Resources"
+
+@"
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="$STTheme"/>
+</head>
+
+<body>
+
+<h1>$PageCaption</h1>
+
+<table id=table2>
+    <tr>
+        <td width="50%">
+            <ul class="ulmenu">
+	            <li class="limain"><a href="https://www.windows-noob.com/" target="_new" title="Windows-Noob">Windows-Noob</a></li>
+	            <li class="limain"><a href="https://scconfigmgr.com/" target="_new" title="SCConfigMgr">SCConfigMgr</a></li>
+	            <li class="limain"><a href="https://patchmypc.com" target="_new" title="Patch My PC">Patch My PC</a> / <a href="https://setupconfigmgr.com" target="_new">Setup ConfigMgr</a></li>
+	            <li class="limain"><a href="https://home.configmgrftw.com" target="_new" title="ConfigMgrFTW">ConfigMgrFTW</a></li>
+	            <li class="limain"><a href="https://deploymentresearch.com" target="_new" title="Deployment Research">Deployment Research</a></li>
+	            <li class="limain"><a href="https://deploymentbunny.com" target="_new" title="Deployment Bunny">Mikael Nystrom</a></li>
+	            <li class="limain"><a href="https://configmgrblog.com/" target="_new" title="ConfigMgr Blog">Peter Daalmans</a></li>
+	            <li class="limain"><a href="http://blogs.technet.com/b/mniehaus/" target="_new" title="Michael Niehaus">Michael Niehaus</a></li>
+	            <li class="limain"><a href="http://maikkoster.com/" target="_new" title="Maik Koster">Maik Koster</a></li>
+	            <li class="limain"><a href="https://reddit.com/r/sccm" target="_new" title="Reddit SCCM">Reddit/SCCM</a></li>
+	            <li class="limain"><a href="https://skatterbrainz.wordpress.com" target="main" title="Skatterbrainz">SkatterBrainz Blog</a></li>
+            </ul>
+        </td>
+        <td width="50%">
+        
+        </td>
+    </tr>
+</table>
+
+</body>
+</html>
+"@
