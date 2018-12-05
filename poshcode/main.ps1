4960c1db90aea1cb3d9f9896d77cc2055de0f3d9
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-04 21:45:51 -0500

    Create main.ps1

---
+++ poshcode/main.ps1
@@ -0,0 +1,15 @@
+@"
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="$STTheme"/>
+</head>
+
+<body>
+
+<h1>Welcome $PoshUserName !</h1>
+
+<img src="splash1.png" border="0" alt="" />
+
+</body>
+</html>
+"@
