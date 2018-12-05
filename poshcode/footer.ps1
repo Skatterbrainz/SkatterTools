db38942ee7f16f1eb1061d86a938186aabcf3dd9
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-04 21:45:33 -0500

    Create footer.ps1

---
+++ poshcode/footer.ps1
@@ -0,0 +1,10 @@
+@"
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="$STTheme"/>
+<head>
+<body style="margin:0;">
+<div class="footer">SkatterTools $STVersion - <a href="license.htm" target="main" title="License">License</a> - <a href="about.ps1" target="main" title="About">About</a></div>
+</body>
+</html>
+"@
