687f414f85ee49f3b19e3ed93da0c8de239f629c
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-15 01:12:30 -0500

    Add files via upload

--- poshserver/adbrowser.ps1
+++ poshserver/adbrowser.ps1
@@ -23,20 +23,63 @@ else {
     $oulist = Get-ADsOUTree | Where {$_.ChildPath.Length -eq 1}
 }
 $rowcount = 0
-$content = "<table id=table1>"
-$content += "<tr><th>Name</th><th>Path</th></tr>"
+if ($SearchValue -ne "") {
+    $content = "<h4>$SearchValue</h4>"
+}
+else {
+    $content = "<h4>root:</h4>"
+}
+$content += "<table style=`"width:100%; border=0`"><tr>"
+$content += "<td style=`"width:250px;vertical-align:top`">"
+$content += "<table id=table1>"
+$content += "<tr><th>Name</th></tr>"
 foreach ($ou in $oulist) {
     $ouname = $ou.Name
     $fpath  = $ou.FullPath
     $cdist  = $ou.ChildPath.Length
-    $xlink  = "<a href=`"adbrowser.ps1?f=FullPath&v=$fpath`" title=`"Explore`">$ouname</a>"
-    $content += "<tr><td>$xlink</td><td>$fpath ($cdist)</td></tr>"
+    if ($SearchValue -ne "" -and $cdist -eq 1) {
+        $xlink = "<a href=`"adbrowser.ps1`">&lt; back...</a>"
+    }
+    elseif ($fpath -eq $SearchValue) {
+        $spath = ($fpath -replace "$ouname", "").TrimEnd('/')
+        $xlink = "<a href=`"adbrowser.ps1?f=FullPath&v=$spath`">&lt; back...</a>"
+    }
+    else {
+        $xlink = "<a href=`"adbrowser.ps1?f=FullPath&v=$fpath`" title=`"Explore`">$ouname</a>"
+    }
+    #$content += "<tr><td>$xlink</td><td>$fpath ($cdist)</td></tr>"
+    $content += "<tr><td>$xlink</td></tr>"
     $rowcount++
 }
-$content += "<tr><td colspan=2 class=lastrow>$rowcount found</td></tr>"
+$content += "<tr><td class=lastrow>$rowcount found</td></tr>"
 $content += "</table>"
-
-$content += Write-DetailInfo -PageRef "adbrowser.ps1" -Mode $Detailed
+$content += "</td><td style=`"vertical-align:top`">"
+try {
+    # convert "contoso.local/CORP" to "ou=CORP,dc=contoso,dc=local"
+    $pathset = ($SearchValue -split '/')
+    # convert "contoso.local" to "dc=contoso,dc=local"
+    $domset  = ($pathset[0].Split('.') | %{"dc=$_"}) -join ','
+    # convert ("CORP","Workstations") to "ou=workstations,ou=corp"
+    $tailset = $pathset[1..($pathset.Length -1)]
+    [array]::Reverse($tailset)
+    $tailset = ($tailset | %{"ou=$_"}) -join ','
+    $oupath  = "$tailset,$domset"
+    $items = Get-AdOuObjects -ou $oupath
+    $content += "<table id=table1>"
+    $xlist = @('Organizational-Unit','Service-Connection-Point')
+    foreach ($item in $items) {
+        if ($item.path -eq $oupath) {
+#        if ($item.name -ne $ouname -and $item.type -notin $xlist) {
+            $content += "<tr><td>$($item.name)</td><td>$($item.type)</td></tr>"
+        }
+    }
+    $content += "</table>"
+}
+catch {}
+finally {
+    $content += "</td></tr></table>"
+    #$content += Write-DetailInfo -PageRef "adbrowser.ps1" -Mode $Detailed
+}
 
 @"
 <html>
