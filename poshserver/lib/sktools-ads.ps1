fdd5216a19485e997529977e4421f6fc7e7b38dc
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-15 01:11:58 -0500

    Add files via upload

--- poshserver/lib/sktools-ads.ps1
+++ poshserver/lib/sktools-ads.ps1
@@ -1,4 +1,4 @@
-﻿$Global:SkToolsLibADS = "1.0.0"
+﻿$Global:SkToolsLibADS = "1.0.2"
 
 function Get-AdsUsers {
     [CmdletBinding()]
@@ -314,5 +314,36 @@ function Get-ADsOUTree {
     }
     catch {
         throw $Error[0].Exception.Message
+    }
+}
+
+function Get-AdOuObjects {
+    [CmdletBinding()]
+    param (
+        [parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string] $ou
+    )
+    $root = [ADSI]"LDAP://$ou"
+    $search = [adsisearcher]$root
+    #$search.Filter = "(&(objectclass=user)(objectcategory=user))"
+    $search.SizeLimit = 3000
+    $results = $search.FindAll()
+    foreach ($result in $results) {
+        $props = $result.Properties
+        foreach ($p in $props) {
+            $itemName = ($p.name | Out-String).Trim()
+            $itemPath = ($p.distinguishedname | Out-String).Trim()
+            $itemPth  = $itemPath -replace "CN=$itemName,", ''
+            $itemType = (($p.objectcategory -split ',')[0]) -replace 'CN=', ''
+            $output = [ordered]@{
+                Name = $itemName
+                DN   = $itemPath
+                Path = $itemPth
+                Type = $itemType
+            }
+            New-Object PSObject -Property $output
+            #select @{N="Name"; E={$_.name}}, @{N="DistinguishedName"; E={$_.distinguishedname}}, @{N="Class"; E={($_.objectcategory -split ',')[0]}}
+        }
     }
 }
