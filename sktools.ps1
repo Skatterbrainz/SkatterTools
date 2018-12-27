7a962366e36365f06ef8b2a8d0a29f0542b8e687
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-27 13:33:22 -0500

    Add files via upload

---
+++ sktools.ps1
@@ -0,0 +1,28 @@
+﻿# SkatterTools Site Configuration
+
+$Global:SkToolsVersion = "1812.27.01"
+
+$configFile = Join-Path -Path $HomeDirectory -ChildPath "config.txt"
+if (!(Test-Path $configFile)) {
+    Write-Warning "Config.txt was not found. Shit just got real."
+    break
+}
+$cdata = Get-Content $configFile | Where-Object{$_ -notlike ';*'}
+foreach ($line in $cdata) {
+    $varset = $line -split '='
+    if ($varset.Count -gt 1) {
+        Set-Variable -Name $varset[0] -Value $($varset[1]).Trim() -Scope Global | Out-Null
+    }
+}
+
+if ($Global:SkToolsLoaded -ne 1) {
+    try {
+        Get-ChildItem (Join-Path -Path $HomeDirectory -ChildPath "lib") -Filter "*.ps1" -ErrorAction Stop | ForEach-Object { . $_.FullName }
+        $Global:SkToolsLoaded = 1
+        $Global:LastLoadTime = Get-Date
+    }
+    catch {
+        Write-Error "OMFG - something smells really bad in here?!"
+        break
+    }
+}
