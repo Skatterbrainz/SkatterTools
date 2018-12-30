bc85187a1c4cecbe126bcc043266a4eb06ca7d93
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2018-12-29 22:00:46 -0500

    Add files via upload

---
+++ poshserver/queries/cmcompstat.sql
@@ -0,0 +1,42 @@
+﻿/*  https://docs.microsoft.com/en-us/sccm/develop/reference/core/servers/manage/sms_statusmessage-server-wmi-class */
+select distinct 
+    RecordID,
+    Component,
+    MachineName,
+    ModuleName,
+    Win32Error,
+    Time,
+    SiteCode,
+    TopLevelSiteCode,
+    ProcessID,
+    ThreadID,
+    case 
+        when (ABS(Severity) = 1073741824) then 'Info'
+        when (ABS(Severity) = 2147483648) then 'Warning'
+        else 'Error' end as Severity,
+    MessageID,
+    case 
+        when (MessageType = 256) then 'Milestone'
+        when (MessageType = 512) then 'Detail'
+        when (MessageType = 768) then 'Audit'
+        when (MessageType = 1024) then 'NTEvent'
+        end as MessageType,
+    case 
+        when (ReportFunction = 0) then 'Report'
+        when (ReportFunction = 16) then 'BeginTransaction'
+        when (ReportFunction = 32) then 'CommitSuccessfulTransaction'
+        when (ReportFunction = 48) then 'CommitFailedTransaction'
+        when (ReportFunction = 64) then 'RollbackTransaction'
+        when (ReportFunction = 80) then 'ReportEX'
+        end as ReportFunction,
+    SuccessfulTransaction,
+    case 
+        when (PartOfTransaction = 0) then 'False'
+        else 'True' end as [Transaction],
+    case 
+        when (PerClient = 0) then 'False'
+        else 'True' end as PerClient
+from 
+    vStatusMessages
+where 
+    DATEDIFF(HH,vStatusMessages.Time, GETDATE()) < 24
---
+++ poshserver/queries/cmscript.sql
@@ -0,0 +1,21 @@
+﻿select distinct
+    ScriptName,
+    ScriptVersion,
+    ScriptGuid,
+    Author,
+    ScriptType,
+    Feature,
+    ApprovalState,
+    CASE 
+        when (ApprovalState = 0) then 'Pending'
+        when (ApprovalState = 1) then 'Denied'
+        when (ApprovalState = 3) then 'Approved'
+        else 'Unknown'
+        end as Approval,
+    Approver,
+    '(It looks like Chinese writing, so I cant display it yet)' as [Script],
+    ScriptHashAlgorithm,
+    ScriptHash,
+    LastUpdateTime,
+    Comment 
+FROM vSMS_Scripts 
